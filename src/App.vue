19d5e7d92f48022b4b398fedbd0ecdb0414afce5
xojiakbar <103825059+xojiakbarsiddikov@users.noreply.github.com>
2022-10-30 08:59:09 +0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -135,51 +135,6 @@ export default {
       check.classList.toggle('colore')
       text.classList.toggle('colore')
     })
-
-    window.addEventListener('load', function(){
-
-      var touchsurface = document.querySelector('.popup-container'),
-          startX,
-          startY,
-          dist,
-          threshold = 150, //required min distance traveled to be considered swipe
-          allowedTime = 200, // maximum time allowed to travel that distance
-          elapsedTime,
-          startTime
-
-      function handleswipe(isrightswipe){
-        if (isrightswipe)
-          touchsurface.innerHTML = 'Congrats, you\'ve made a <span style="color:red">right swipe!</span>'
-        else{
-          touchsurface.innerHTML = 'Condition for right swipe not met yet'
-        }
-      }
-
-      touchsurface.addEventListener('touchstart', function(e){
-        touchsurface.innerHTML = ''
-        var touchobj = e.changedTouches[0]
-        dist = 0
-        startX = touchobj.pageX
-        startY = touchobj.pageY
-        startTime = new Date().getTime() // record time when finger first makes contact with surface
-        e.preventDefault()
-      }, false)
-
-      touchsurface.addEventListener('touchmove', function(e){
-        e.preventDefault() // prevent scrolling when inside DIV
-      }, false)
-
-      touchsurface.addEventListener('touchend', function(e){
-        var touchobj = e.changedTouches[0]
-        dist = touchobj.pageX - startX // get total dist traveled by finger while in contact with surface
-        elapsedTime = new Date().getTime() - startTime // get time elapsed
-        // check that elapsed time is within specified, horizontal dist traveled >= threshold, and vertical dist traveled <= 100
-        var swiperightBol = (elapsedTime <= allowedTime && dist >= threshold && Math.abs(touchobj.pageY - startY) <= 100)
-        handleswipe(swiperightBol)
-        e.preventDefault()
-      }, false)
-
-    }, false) // end window.onload
   },
 };
 </script>
